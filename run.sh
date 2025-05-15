4bb607aea87217cd8470770d31f652d3b1732e1b
avijayasarathy <avijayasarathy@calamp.com>
2025-05-14 18:49:21 -0700

    Replaced server list with hashmap

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-# launch.sh
-java SocketServer node01 &
-java SocketServer node02 &
-java SocketServer node03 &
-java SocketServer node04 &
+# run.sh
+# Compile all Java files first
+javac *.java
+
+# Run the application with different node IDs
+java App node01 &
+java App node02 &
+java App node03 &
+java App node04 &
